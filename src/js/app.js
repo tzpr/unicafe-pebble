fe06e7e92933382c4a51eb36a5601ffb0e9f4b94
Toni Räsänen <toni.rasanen@iki.fi>
2017-11-08 22:56:03 +0200

    config page

--- src/js/app.js
+++ src/js/app.js
@@ -8,8 +8,16 @@ var Clock = require('clock'),
     uniCafes = require('unicafe-modules/cafe-container'),
     lunchMenu = require('unicafe-modules/lunch-handler'),
     log = require('unicafe-modules/uni-util').log,
-    shortVibeNotification = require('unicafe-modules/uni-util').shortVibe;
+    shortVibeNotification = require('unicafe-modules/uni-util').shortVibe,
+    Clay = require('clay'),
+    clayConfig = require('config.json');
 
+
+var clay = new Clay(clayConfig);
+
+//Clay.getItemsById();
+
+//log(' *** CONFIG: ' + Clay.getItemsById());
 
 var main = new UI.Menu({
     sections: [{
